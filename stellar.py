9428dffeface7e9a4df43ba96d2241e84f1c235e
brupoon <disc325@gmail.com>
2015-06-12 19:44:32 -0700

    Final work submit, june 12 2015
    final work submit. worked on crop.

--- stellar.py
+++ stellar.py
@@ -25,8 +25,37 @@ def converter(imageToConvert):
     print(imageArray.dtype)
     
     return imageArray
-    
-def crop(image):
+                
+def crop(image): #Working from previous code now contained in oldcrop
+    """
+    Crops an image img based on the number of empty pixels [0,0,0]
+    Crops top-to-bottom, right-to-left, bottom-to-top, and then left-to-right
+    based on the way that the current set of data has been collected.
+    """
+    """
+    The importation of the tif using PIL works like this: Each row in the
+    array array[i] is a list of lists. Each list in the list (array[i][j]) is
+    a list of 3 values where each value refers to the RGB value (HAVE TO FIND
+    OUT IF IT IS IN THAT ORDER).
+    """
+    incrementer = False
+    counterPerRow = 0
+    duplicate = image
+    for i in range(len(duplicate[0][i])-1):
+        if duplicate[0][i] == [0,0,0]:
+            duplicate = np.delete(duplicate, 0, 0)
+            #TODO something interesting to think about -- could it be that
+            #noise from the detector is going to prevent a row of perfect zeros?
+
+#TODO write a function which will determine the greatest singluar value
+#in our array so that we can see how much tolerance we have of deleting
+#1s, 2s, 3s etc.
+
+            
+def oldcrop(image):
+    """
+    OLD OLD CODE CODE DO NOT USE
+    """
     """
     Crops an image img based on the number of empty pixels [0,0,0]
     Crops top-to-bottom, right-to-left, bottom-to-top, and then left-to-right
@@ -46,8 +75,9 @@ def crop(image):
             for j in range(len(image[i])-1): #per pixel
                 if image[i][j] == [0,0,0]:
                     counterPerRow = counterPerRow + 1
-            if counterPerRow = len(image[i]):
+            if counterPerRow == len(image[i]):
                 #delete duplicate top row.
                 #how to do this without messing up the count
                 #always test the top row? that could do it, instaed of using
                 #a metric blah of for loopings.
+                pass
--- tests.py
+++ tests.py
@@ -14,3 +14,12 @@ file = 'IMG_2860.tif'
 
 fileArray = converter(file)
 writeLogToFile(fileArray)
+
+def testArray(nparray):
+    """
+    This will create a test array with the same dimensions as inputted nparray
+    """
+    returner = np.zeros_like(nparray)
+    #need to put random non-zero values in certain places so that we can make
+    #sure that the crop loop is terminating where it should
+    return returner
