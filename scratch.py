798f60ce729a665b987449029e48d56d7449f998
brupoon <disc325@gmail.com>
2015-06-16 18:10:48 -0700

    Work 2015.06.16
    Digitized work log, identified issues around the crop() function, and
    implemented a (flawed) vertical crop method, among other previously
    mentioned changes made earlier in the day.

--- scratch.py
+++ scratch.py
@@ -23,3 +23,11 @@ print("test[:,15]:\n", test[:,15])
 #SUCCESS! This will produce the 15th column across all the rows. This is great
 #because not only will it work for cropping but it  will also help in adding
 #all of the pixel values in the column.
+
+#TIF importation using pillow/PIL
+"""
+The importation of the tif using PIL works like this: Each row in the
+array array[i] is a list of lists. Each list in the list (array[i][j]) is
+a list of 3 values where each value refers to the RGB value (HAVE TO FIND
+OUT IF IT IS IN THAT ORDER).
+"""
--- stellar.py
+++ stellar.py
@@ -32,29 +32,36 @@ def crop(image): #Working from previous code now contained in oldcrop
     Crops top-to-bottom, right-to-left, bottom-to-top, and then left-to-right
     based on the way that the current set of data has been collected.
     """
-    """
-    The importation of the tif using PIL works like this: Each row in the
-    array array[i] is a list of lists. Each list in the list (array[i][j]) is
-    a list of 3 values where each value refers to the RGB value (HAVE TO FIND
-    OUT IF IT IS IN THAT ORDER).
-    """
-    incrementer = False
-    duplicate = image
-    for j in range(len(duplicate)-1): #goes by line
+
+    duplicate = np.copy(image) #working with NEW ARRAY not REFERENCING OLD!!!!
+
+    #cropping horizontally
+    for j in range(len(duplicate)): #goes by row
         counterPerRow = 0
         #what is up with the first for loop being -1
         #and the second just left
-        for i in range(len(duplicate[j])): #goes py pixel in line
+        for i in range(len(duplicate[j])): #goes by pixel in row
             if np.array_equal(duplicate[j][i], np.array([0,0,0])):
                 #adds to counter if pixel is empty
                 counterPerRow = counterPerRow + 1
-                if counterPerRow == len(duplicate[0]):
-                    #if whole row is empty, delete the row in question
-                    duplicate = np.delete(duplicate, j, 0)
-                    print("for loopcount:", i)
-                    print("counterPerRow value:", counterPerRow)
+            if counterPerRow == len(duplicate[0]):
+                #if whole row is empty, delete the row in question
+                duplicate = np.delete(duplicate, j, 0)
             #TODO something interesting to think about -- could it be that
             #noise from the detector is gonna prevent a row of perfect zeros?
+
+    #cropping vertically
+    for k in range(len(duplicate[0])-1): #goes by column
+        counterPerColumn = 0
+        vertSlice = duplicate[:,k] # a single column k with all rows
+        print("vertSlice:\n", vertSlice)
+        for element in vertSlice:
+            if np.array_equal(element, np.array([0,0,0])):
+                counterPerColumn = counterPerColumn + 1
+                print("counterPercolumn", counterPerColumn)
+        if counterPerColumn == len(vertSlice):
+            duplicate = np.delete(duplicate, k, 1)
+
     return duplicate
 #TODO write a function which will determine the greatest singluar value
 #in our array so that we can see how much tolerance we have of deleting
