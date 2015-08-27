fixed calculation for box in intensitySAANB

stellar.py
@@ -150,6 +150,8 @@ def intensitySAANB(img, data, reg, threshold=127, r=1, twidth=10):
     Returns a dictionary where key is x value and y
     value is intensity.
     r is the step rate along the spectral trace (default to size of one pixel)
+    t is the width of the trace on each side of the line y = mx + c
+    so the total will be double
     """
     
     lowerx, lowery, upperx, uppery = img.getbbox()
@@ -167,10 +169,10 @@ def intensitySAANB(img, data, reg, threshold=127, r=1, twidth=10):
         ypixel = m * xpixel + c
         for newx in np.arange(lowerx, upperx - 1, 0.1):
             newy = n * (newx - xpixel) + ypixel #point-slope, add ypixel ea.side
-            #We want to add spectral twidth functionality
+            #We want to add spectral trace width functionality
             if (newy > (ypixel-twidth)) or (newy < (ypixel+twidth)):
                 #anti-aliasing implementation http://is.gd/dnj08y
-                for newxRounded in (math.floor(newx-r), math.ceil(newx+r)):
+                for newxRounded in (math.floor(newx), math.ceil(newx+r)):
                     for newyRounded in (math.floor(newy), math.ceil(newy)):
                         #we need to be sure that the rounded point is in our img
                         if (newyRounded > lowery) and (newyRounded < uppery)\
@@ -197,52 +199,52 @@ def intensitySAANB(img, data, reg, threshold=127, r=1, twidth=10):
     if v=='yes': print("intensities:", intensities)
     return intensities
 
-# def intensitySAANB(img, data, reg, threshold=127, r=1,twidth=10):
-#     """
-#     intensitySAANB is the fourth iteration of the intensity function which aims
-#     to deal with the plotting of regressed non-orthogonal spectra given in
-#     an open image img, the pixel data in data, and a regArray generated
-#     using regression().
-#     SAA - spatial antialiasing; NB - new, box method
-#     Returns a dictionary where key is x value and y
-#     value is intensity.
-#     r is the step rate along the spectral trace (default to size of one pixel)
-#     """
+def notintensitySAANB(img, data, reg, threshold=127, r=1,twidth=10):
+    """
+    intensitySAANB is the fourth iteration of the intensity function which aims
+    to deal with the plotting of regressed non-orthogonal spectra given in
+    an open image img, the pixel data in data, and a regArray generated
+    using regression().
+    SAA - spatial antialiasing; NB - new, box method
+    Returns a dictionary where key is x value and y
+    value is intensity.
+    r is the step rate along the spectral trace (default to size of one pixel)
+    """
     
-#     lowerx, lowery, upperx, uppery = img.getbbox()
-#     m, c = reg[0:2]
-#     n = -1 / m
-#     #background subtraction median calculation
-#     back = backMedian(img, threshold)
-#     if v=='yes': print("backMedian:", back)
-#     print("running intensitySAAN")
-#     intensities = {} #this is a dictionary.
-#     step = math.sqrt((r**2) / (1 + m**2))
-#     #generate the coordinate arrays for our image
-#     xArr = np.arange(upperx+1)
-#     yArr = np.arange(uppery+1)
-#     lineM = np.multiply(m, xArr)
-#     lineM = np.add(c, lineM)
-#     r = 0.5 * r
-#     for pointX in np.linspace(lowerx, upperx,num=math.ceil((upperx/step)+1)):
-#         #pointY = n * (pointX - 
-#         for y in range(uppery+1):
+    lowerx, lowery, upperx, uppery = img.getbbox()
+    m, c = reg[0:2]
+    n = -1 / m
+    #background subtraction median calculation
+    back = backMedian(img, threshold)
+    if v=='yes': print("backMedian:", back)
+    print("running intensitySAAN")
+    intensities = {} #this is a dictionary.
+    step = math.sqrt((r**2) / (1 + m**2))
+    #generate the coordinate arrays for our image
+    xArr = np.arange(upperx+1)
+    yArr = np.arange(uppery+1)
+    lineM = np.multiply(m, xArr)
+    lineM = np.add(c, lineM)
+    r = 0.5 * r
+    for pointX in np.linspace(lowerx, upperx,num=math.ceil((upperx/step)+1)):
+        #pointY = n * (pointX - 
+        for y in range(uppery+1):
 
-#             pointIndices = np.where((yArr[y] <= (m * xArr + twidth)) &\
-#                                     (yArr[y] >= (m * xArr - twidth)) &\
-#                                     (xArr[y] <= (n * xArr + r)) &\
-#                                     (xArr[y] >= (n * xArr - r)))
-#             print("pointIndices for {0}: ".format(y), pointIndices)
-#             for point in pointIndices:
-#                 if pointX in intensities:
-#                     intensities[pointX] = intensities[pointX] + point
-#                 else:
-#                     intensities[pointX] = point
-#                 #todo intensities[pointX] -= percent * back DEAL WITH THIS
+            pointIndices = np.where((yArr[y] <= (m * xArr + twidth)) &\
+                                    (yArr[y] >= (m * xArr - twidth)) &\
+                                    (xArr[y] <= (n * xArr + r)) &\
+                                    (xArr[y] >= (n * xArr - r)))
+            print("pointIndices for {0}: ".format(y), pointIndices)
+            for point in pointIndices:
+                if pointX in intensities:
+                    intensities[pointX] = intensities[pointX] + point
+                else:
+                    intensities[pointX] = point
+                #todo intensities[pointX] -= percent * back DEAL WITH THIS
 
-#         to.pbar(pointX/upperx) #progress bar
-#     if v=='yes': print("intensities:", intensities)
-#     return intensities
+        to.pbar(pointX/upperx) #progress bar
+    if v=='yes': print("intensities:", intensities)
+    return intensities
 
 def sumGenerator(data):
     """